ce0a10a7d0c3e03120d0c6d6637a5d7b577ba597
Alexandro Silva <alexandro.silva@embasa.ba.gov.br>
2025-04-10 21:14:20 +0000

    feat: Add validation to ensure FirstName and LastName are not empty

--- src/Common/Domain/ValueObject/FirstName.php
+++ src/Common/Domain/ValueObject/FirstName.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace App\Common\Domain\ValueObject;
 
+use App\Common\Domain\Exception\InvalidFormat;
+
 final class FirstName extends StringValue
 {
+    protected function __construct(string $value)
+    {
+        parent::__construct($value);
+        $this->ensureIsNotEmpty();
+    }
+
+    private function ensureIsNotEmpty(): void
+    {
+        if (trim($this->value) === '') {
+            throw new InvalidFormat('First name cannot be empty');
+        }
+    }
 }
--- src/Common/Domain/ValueObject/LastName.php
+++ src/Common/Domain/ValueObject/LastName.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace App\Common\Domain\ValueObject;
 
+use App\Common\Domain\Exception\InvalidFormat;
+
 final class LastName extends StringValue
 {
+    protected function __construct(string $value)
+    {
+        parent::__construct($value);
+        $this->ensureIsNotEmpty();
+    }
+
+    private function ensureIsNotEmpty(): void
+    {
+        if (trim($this->value) === '') {
+            throw new InvalidFormat('Last name cannot be empty');
+        }
+    }
 }
